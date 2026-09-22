f826c2760222695c9d06cdac9ef8a5f799f8b8e5
olise-creator <2586155@cegepoutaouais.qc.ca>
2026-09-22 14:46:39 -0400

    Création de la classe Compte

---
+++ Magasin/Compte.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magasin
+{
+    public class Compte
+    {
+        private decimal solde;
+        public decimal Solde
+        {
+            get { return this.solde; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("La valeur ne peut pas être négative!");
+                }
+                this.solde = value;
+            }
+        }
+
+        public Compte() : this(0)
+        {
+
+        }
+
+        public Compte(decimal soldeDepart)
+        {
+            this.solde = soldeDepart;
+        }
+
+        public void Depot(decimal montant)
+        {
+            this.Solde += montant;
+        }
+
+        public void Retrait(decimal montant)
+        {
+            this.Solde -= montant;
+        }
+
+        public override string ToString()
+        {
+            return $"Compte bancaire qui possède {this.solde:F2}$";
+        }
+    }
+
+    public class CompteHerite : Compte
+    {
+        public string Couleur { get; set; }
+
+    }
+
+}
